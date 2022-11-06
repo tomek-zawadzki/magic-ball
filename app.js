9351334414c30ac05a06e39f3b374bec7b23b97d
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-06 18:15:26 +0100

    add function to shake ball

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const errorMsg = document.querySelector(".error");
 const answers = [
   "TAK",
   "NIE.",
+  "MOżE",
   "CIEZKO POWIEDZIEĆ...",
   "NIE CHCESZ ZNAĆ ODPOWIEDZI NA TO PYTANIE",
 ];
+
+const shakeBall = () => {
+  ball.classList.add("shake-animation");
+  setTimeout(checkInput, 1000);
+};
 
 const generateAnswer = () => {
   const number = Math.floor(Math.random() * 5);
   answer.innerHTML = `<span>Odpowiedź:<span> ${answers[number]}`;
 };
 
-ball.addEventListener("click", () => {
+const checkInput = () => {
   if (question.value !== "" && question.value.slice(-1) === "?") {
     generateAnswer();
     errorMsg.textContent = "";
@@ -26,4 +32,7 @@ ball.addEventListener("click", () => {
     errorMsg.textContent = "Musisz zadać pytanie";
     answer.textContent = "";
   }
-});
+  ball.classList.remove("shake-animation");
+};
+
+ball.addEventListener("click", shakeBall);
